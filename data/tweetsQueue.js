15f1a91255aa8cdc1e610bc8564e1fca9b7fa5c5
xiaohangsu <sxhdragon@gmail.com>
2017-03-03 01:44:11 -0500

    Remove bug from tweetsQueue

--- data/tweetsQueue.js
+++ data/tweetsQueue.js
@@ -1,60 +1,59 @@
 var tweetsQueue = ()=> {
-	var tweet = (json)=> {
-		return {
-			id: json.id,
-			text: json.text,
-			"created_at": json["created_at"].split("+")[0],
-			coordinates: json.coordinates.coordinates,
-			user: {
-				"screen_name": json.user["screen_name"],
-				"profile_image_url": json.user["profile_image_url_https"]
-			}
-		}
-	};
+    var tweet = (json)=> {
+        return {
+            id: json["id_str"],
+            text: json.text,
+            "created_at": json["created_at"].split("+")[0],
+            coordinates: json.coordinates.coordinates,
+            user: {
+                "screen_name": json.user["screen_name"],
+                "profile_image_url": json.user["profile_image_url_https"]
+            }
+        }
+    };
 
-	var removeOldestTweetFromQueue = ()=> {
-		delete this.tweets[this.tweetsKey[0]];
-		this.tweetsKey.shift();
-	}
+    var removeOldestTweetFromQueue = ()=> {
+        delete this.tweets[this.tweetsKey[0]];
+        this.tweetsKey.shift();
+    }
 
 
-	// tweets key = id, value can be text and coordinates
-	this.tweets = {};
-	// store all the keys of tweetsQueue
-	this.tweetsKey = [];
-	this.limits = 500;
-	this.addTweet = (json)=> {
-		if (this.tweets[json.id] == undefined) {
-			this.tweets[json.id] = tweet(json);
-			this.tweetsKey.push(json.id);
-			if (this.tweetsKey.length == this.limits) {
-				removeOldestTweetFromQueue();
-			}
+    // tweets key = id, value can be text and coordinates
+    this.tweets = {};
+    // store all the keys of tweetsQueue
+    this.tweetsKey = [];
+    this.limits = 500;
+    this.addTweet = (json)=> {
+        if (this.tweets[json.id] == undefined) {
+            this.tweets[json.id] = tweet(json);
+            this.tweetsKey.push(json.id);
+            if (this.tweetsKey.length == this.limits) {
+                removeOldestTweetFromQueue();
+            }
+        }
+    }
 
-		}
-	}
+    this.getTopk = (k)=> {
+        tweetsList = {};
+        var len = this.tweetsKey.length;
+        for (var i = len > k ? len - k : 0; i < len; i++) {
+            tweetsList[this.tweetsKey[len - i + 1]] = this.tweets[this.tweetsKey[len - i + 1]];
+        }
+        return tweetsList;
+    }
 
-	this.getTopk = (k)=> {
-		tweetsList = {};
-		var len = this.tweetsKey.length;
-		for (var i = len > k ? len - k : 0; i < len; i++) {
-			tweetsList[this.tweetsKey[len - i + 1]] = this.tweets[this.tweetsKey[len - i + 1]];
-		}
-		return tweetsList;
-	}
-
-	this.getNewTweetsDownToId = (pastId, max = 20)=> {
-		tweetsList = {};
-		var len = this.tweetsKey.length;
-		for (var i = len < max ? 0 : len - max; i < len; i++) {
-			tweetsList[this.tweetsKey[i]] = this.tweets[this.tweetsKey[i]];
-			if (pastId == this.tweetsKey[i]) {
-				tweetsList = {};
-			}
-		}
-		return tweetsList;
-	}
-	return this;
+    this.getNewTweetsDownToId = (pastId, max = 20)=> {
+        tweetsList = {};
+        var len = this.tweetsKey.length;
+        for (var i = len < max ? 0 : len - max; i < len; i++) {
+            tweetsList[this.tweetsKey[i]] = this.tweets[this.tweetsKey[i]];
+            if (pastId == this.tweetsKey[i]) {
+                tweetsList = {};
+            }
+        }
+        return tweetsList;
+    }
+    return this;
 }
 
 module.exports = tweetsQueue();
