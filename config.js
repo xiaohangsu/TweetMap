753ad36096799c83e846412e66c5d94af07a986f
xiaohangsu <sxhdragon@gmail.com>
2017-03-16 11:28:31 -0400

    api delete

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
     tweetOauth: {
-        key: 'yIzrJlgbCpDzlMSmoBd6jKZfo',
-        secret: 'Y89G3sFShAcr3FpzwVzDpKgvkxJ7pYZfzSv2jUP5tzTmhvOYdD',
+        key: '',
+        secret: '',
         token: {
-            key: '760285412455624704-nTz2BQJlvCIA98pI5dkU6DCx7MuquTC',
-            secret: '8NMzq0Zeo7bDgNJeXj5SDGOEAjOmco5RFIC3h0OqcWH0K'
+            key: '',
+            secret: ''
         },
         signatureMethod: 'HMAC-SHA1'
     },
