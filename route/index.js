d6eefe92731913b1accb981bee4676716dc5fc14
Xiaohang Su <sxhdragon@gmail.com>
2017-04-18 00:05:37 -0400

    Upgrade Koa router & add dev mode and dist mode

--- route/index.js
+++ route/index.js
@@ -1,10 +1,20 @@
 const router       = require('koa-router')();
 const send         = require('koa-send');
 const GOOGLEMAPAPI = require('../config').GoogleMapAPI;
+const CONFIG 	   = require('process').argv[2];
 
-router.get('/', function*() {
-    yield this.render('./index', {
-        googleMapAPI: GOOGLEMAPAPI
+let STATIC_PATH = '';
+if (CONFIG === 'dev') {
+	STATIC_PATH = 'http://localhost:8080/';
+} else if (CONFIG === 'dist') {
+	STATIC_PATH = '/dist/';
+}
+
+router.get('/', (ctx, next)=> {
+	console.log('/ GET |');
+    return ctx.render('./index', {
+        googleMapAPI: GOOGLEMAPAPI,
+        staticPath  : STATIC_PATH 
     });
 });
 
--- route/tweet.js
+++ route/tweet.js
@@ -1,41 +1,46 @@
-const router       = require('koa-router')();
-let tweetsStream   = require('../data/tweetsStream');
+const Router       = require('koa-router');
 let tweetsQueue    = require('../data/tweetsQueue');
+const kafkaStream  = require('../data/kafkaStream');
+let router = new Router();
 
-router.get('/tweet/:id', function*() {
-    let id = parseInt(this.params.id);
-    if (tweetsStream.isLostConnection() || !tweetsQueue.hasNew(id)) {
-        tweetsStream.createTweetsStreamingReq();
-        console.log('Create a new Tweet Stream in minutes...');
-    }
+router.get('/tweet/:id', async (ctx, next)=> {
+    let id = parseInt(ctx.params.id);
+    // if (tweetsStream.isLostConnection() || !tweetsQueue.hasNew(id)) {
+    //     tweetsStream.createTweetsStreamingReq();
+    //     console.log('Create a new Tweet Stream in minutes...');
+    // }
 
     if (isNaN(id)) {
-        this.body = yield tweetsQueue.getTopk(100);
+        ctx.body = await tweetsQueue.getTopk(100);
     } else {
-        this.body = yield tweetsQueue.getNewTweetsDownToId(id);
+        ctx.body = await tweetsQueue.getNewTweetsDownToId(id);
     }
 
 
-}).get('/tweetDetail/:id', function*() {
-    let id = parseInt(this.params.id);
+}).get('/tweetDetail/:id', async (ctx, next)=> {
+    let id = parseInt(ctx.params.id);
     if (!isNaN(id)) {
-        this.body = yield tweetsQueue.getTweetDetail(this.params.id);
+        ctx.body = await tweetsQueue.getTweetDetail(ctx.params.id);
     } else {
         console.log('/TweetDetail/'+ id + ' NOT FOUND');
+        ctx.body = {
+            status: 400,
+            message: 'ERROR'
+        }
     }
 
 
-}).get('/tweet/search/:text', function*() {
-    this.body = yield tweetsQueue.search(this.params.text);
+}).get('/tweet/search/:text', async (ctx, next)=> {
+    ctx.body = await tweetsQueue.search(ctx.params.text);
 
-}).get('/tweet/search/:text/:scrollId', function*() {
-    this.body = yield tweetsQueue.scroll(this.params.scrollId);
+}).get('/tweet/search/:text/:scrollId', async (ctx, next)=> {
+    ctx.body = await tweetsQueue.scroll(ctx.params.scrollId);
 
-}).get('/tweet/searchGeo/:dis/:coord', function*() {
-    this.body = yield tweetsQueue.searchGeo(this.params.dis, this.params.coord);
+}).get('/tweet/searchGeo/:dis/:coord', async (ctx, next)=> {
+    ctx.body = await tweetsQueue.searchGeo(ctx.params.dis, ctx.params.coord);
 
-}).get('/tweet/searchGeo/:dis/:coord/:scrollId', function*() {
-    this.body = yield tweetsQueue.scroll(this.params.scrollId);
+}).get('/tweet/searchGeo/:dis/:coord/:scrollId', async (ctx, next)=> {
+    ctx.body = await tweetsQueue.scroll(ctx.params.scrollId);
 });
 
 module.exports = router;
